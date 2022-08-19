4d8f5f5f23882ae6f8e82dc50548a4b2aec73d5b
hudson <ganimedes409@gmail.com>
2022-08-19 16:16:32 -0300

    Configuracao do useContext

---
+++ src/context/recipesContext.jsx
@@ -0,0 +1,20 @@
+import React, { createContext, useContext } from 'react';
+import PropTypes from 'prop-types';
+
+const RecipesContext = createContext({});
+
+export const RecipesProvider = ({ children }) => {
+  const hh = '';
+  return (
+    <RecipesContext.Provider value={ {} }>{children}</RecipesContext.Provider>
+  );
+};
+
+export function ContextRecipes() {
+  const context = useContext(RecipesContext);
+  return context;
+}
+
+RecipesProvider.propTypes = {
+  children: PropTypes.node,
+}.isRequired;
