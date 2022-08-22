55ca7bd7c85974fd2db1c861d7e5efe7ec936481
Davi Albino <negroalbino_@hotmail.com>
2022-08-22 19:40:10 -0300

    salvando

--- src/Pages/Foods.jsx
+++ src/Pages/Foods.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Footer from '../components/Footer';
 import Header from '../components/Header';
 
 function Foods() {
   return (
-    <Header titulo="Foods" showBtn />
+    <div>
+      <Header titulo="Foods" showBtn />
+      <Footer />
+    </div>
   );
 }
 
