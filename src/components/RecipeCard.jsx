cec8d79ecddf0f829dc9c513269228fb17e5e72a
hudson <ganimedes409@gmail.com>
2022-08-24 15:56:03 -0300

    Requisito 14 finalizado

---
+++ src/components/RecipeCard.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const RecipeCard = (props) => {
+  const { index, img, name } = props;
+  return (
+    <div data-testid={ `${index}-recipe-card` }>
+      <img src={ img } alt={ name } data-testid={ `${index}-card-img` } />
+      <p data-testid={ `${index}-card-name` }>{name}</p>
+    </div>
+  );
+};
+
+RecipeCard.propTypes = {
+  index: PropTypes.number,
+  img: PropTypes.string,
+  name: PropTypes.string,
+}.isRequired;
+
+export default RecipeCard;
