aa771750cce763b1f7d0c648534872237ca6ea1b
leandrojamir <leandrojamir@hotmail.com>
2022-08-26 11:50:39 -0300

    90% 15 com merge da 21 e 22, testes local 2 a 23 e 57 a 62 ok

---
+++ src/test/mocks/Onion.js
@@ -0,0 +1,136 @@
+const onion = {
+  meals: [
+    {
+      strMeal: ' Bubble & Squeak',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/xusqvw1511638311.jpg',
+      idMeal: '52885',
+    },
+    {
+      strMeal: 'Baingan Bharta',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/urtpqw1487341253.jpg',
+      idMeal: '52807',
+    },
+    {
+      strMeal: 'Beef and Mustard Pie',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/sytuqu1511553755.jpg',
+      idMeal: '52874',
+    },
+    {
+      strMeal: 'Beef Banh Mi Bowls with Sriracha Mayo, Carrot & Pickled Cucumber',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/z0ageb1583189517.jpg',
+      idMeal: '52997',
+    },
+    {
+      strMeal: 'Beef Brisket Pot Roast',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/ursuup1487348423.jpg',
+      idMeal: '52812',
+    },
+    {
+      strMeal: 'Beef Lo Mein',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1529444830.jpg',
+      idMeal: '52952',
+    },
+    {
+      strMeal: 'Big Mac',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/urzj1d1587670726.jpg',
+      idMeal: '53013',
+    },
+    {
+      strMeal: 'Bigos (Hunters Stew)',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/md8w601593348504.jpg',
+      idMeal: '53018',
+    },
+    {
+      strMeal: 'Bitterballen (Dutch meatballs)',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/lhqev81565090111.jpg',
+      idMeal: '52979',
+    },
+    {
+      strMeal: 'Broccoli & Stilton soup',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/tvvxpv1511191952.jpg',
+      idMeal: '52842',
+    },
+    {
+      strMeal: 'Burek',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/tkxquw1628771028.jpg',
+      idMeal: '53060',
+    },
+    {
+      strMeal: 'Callaloo Jamaican Style',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/ussyxw1515364536.jpg',
+      idMeal: '52939',
+    },
+    {
+      strMeal: 'Cevapi Sausages',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/vc08jn1628769553.jpg',
+      idMeal: '53055',
+    },
+    {
+      strMeal: 'Chakchouka ',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/gpz67p1560458984.jpg',
+      idMeal: '52969',
+    },
+    {
+      strMeal: 'Chicken & mushroom Hotpot',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/uuuspp1511297945.jpg',
+      idMeal: '52846',
+    },
+    {
+      strMeal: 'Chicken Alfredo Primavera',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/syqypv1486981727.jpg',
+      idMeal: '52796',
+    },
+    {
+      strMeal: 'Chicken Couscous',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/qxytrx1511304021.jpg',
+      idMeal: '52850',
+    },
+    {
+      strMeal: 'Chicken Fajita Mac and Cheese',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/qrqywr1503066605.jpg',
+      idMeal: '52818',
+    },
+    {
+      strMeal: 'Chicken Handi',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/wyxwsp1486979827.jpg',
+      idMeal: '52795',
+    },
+    {
+      strMeal: 'Chivito uruguayo',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/n7qnkb1630444129.jpg',
+      idMeal: '53063',
+    },
+    {
+      strMeal: 'Clam chowder',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/rvtvuw1511190488.jpg',
+      idMeal: '52840',
+    },
+    {
+      strMeal: 'Corba',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+      idMeal: '52977',
+    },
+    {
+      strMeal: 'Crispy Sausages and Greens',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/st1ifa1583267248.jpg',
+      idMeal: '52999',
+    },
+    {
+      strMeal: 'Croatian lamb peka',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/pn59o51628769837.jpg',
+      idMeal: '53056',
+    },
+    {
+      strMeal: 'Egyptian Fatteh',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/rlwcc51598734603.jpg',
+      idMeal: '53031',
+    },
+    {
+      strMeal: 'Escovitch Fish',
+      strMealThumb: 'https://www.themealdb.com/images/media/meals/1520084413.jpg',
+      idMeal: '52944',
+    },
+  ],
+};
+
+module.exports = onion;
