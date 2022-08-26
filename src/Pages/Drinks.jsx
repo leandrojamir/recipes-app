5800c1bbd77eec267741236c0328db9a71dc8738
hudson <ganimedes409@gmail.com>
2022-08-26 10:20:11 -0300

    Requisito 21 e 22 finalizados

--- src/Pages/Drinks.jsx
+++ src/Pages/Drinks.jsx
@@ -7,26 +7,41 @@ import { ContextRecipes } from '../context/recipesContext';
 import RecipeCard from '../components/RecipeCard';
 
 function Drinks() {
-  const { arrResults, getCategoryDrinks } = ContextRecipes();
+  const {
+    arrResults,
+    getCategoryDrinks,
+    handleClickCategoryDrink,
+    filterCategoryDrinks,
+    handleClickBtnAllDrinks,
+  } = ContextRecipes();
   const maxNumber = 12;
   const maxCategory = 5;
-
+  console.log(filterCategoryDrinks);
   return (
     <>
       <Header titulo="Drinks" showBtn />
       <main>
-        { getCategoryDrinks.length > 0
-        && getCategoryDrinks.slice(0, maxCategory).map((drink) => (
+        <section>
           <button
             type="button"
-            key={ drink.strCategory }
-            name={ drink.strCategory }
-            data-testid={ `${drink.strCategory}-category-filter` }
+            data-testid="All-category-filter"
+            onClick={ handleClickBtnAllDrinks }
           >
-            { drink.strCategory }
+            All
           </button>
-        ))}
-        <Recipes titulo="Drinks" />
+          { getCategoryDrinks.length > 0
+          && getCategoryDrinks.slice(0, maxCategory).map((drink) => (
+            <button
+              type="button"
+              key={ drink.strCategory }
+              name={ drink.strCategory }
+              onClick={ handleClickCategoryDrink }
+              data-testid={ `${drink.strCategory}-category-filter` }
+            >
+              { drink.strCategory }
+            </button>
+          ))}
+        </section>
         <section>
           { arrResults && arrResults.slice(0, maxNumber)
             .map((drink, index) => (
@@ -39,6 +54,28 @@ function Drinks() {
               </Link>
             ))}
         </section>
+        <section>
+          { filterCategoryDrinks
+            && filterCategoryDrinks.slice(0, maxNumber)
+              .map((drink, index) => (
+                <div
+                  data-testid={ `${index}-recipe-card` }
+                  key={ drink.strDrink }
+                >
+                  <img
+                    data-testid={ `${index}-card-img` }
+                    src={ drink.strDrinkThumb }
+                    alt={ drink.strDrink }
+                  />
+                  <p
+                    data-testid={ `${index}-card-name` }
+                  >
+                    { drink.strDrink }
+                  </p>
+                </div>
+              ))}
+        </section>
+        <Recipes titulo="Drinks" />
       </main>
       <Footer />
     </>
