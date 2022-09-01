138bc77644da01817ef67813a48b430247111a8e
leandrojamir <leandrojamir@hotmail.com>
2022-09-01 19:47:23 -0300

    salvando testes da 29 36 52 e 56

---
+++ src/tests/FavoriteRecipes.test.js
@@ -0,0 +1,109 @@
+import React from "react";
+import { screen, waitFor } from '@testing-library/react'
+import renderWithRouter from "./helpers/renderWithRouter";
+import userEvent from '@testing-library/user-event';
+import App from "../App";
+// import FavoriteRecipes from "../pages/FavoriteRecipes";
+// import AppProvider from "../context/AppProvider";
+
+const favoriteRecipes = JSON.stringify([
+  {
+    id: '123',
+    type: 'drink',
+    nationality: '',
+    category: '',
+    alcoholicOrNot: 'alcoholic',
+    name: 'Pinga',
+    image: 'imagem-da-pinga'
+  },
+  {
+    id: '456',
+    type: 'food',
+    nationality: 'brazilian',
+    category: 'comida',
+    alcoholicOrNot: '',
+    name: 'lasanha',
+    image: 'imagem-da-lasanha'
+  }
+]);
+
+describe('Testes com o componente FavoriteRecipes', () => {
+  it('Verifica se ao renderizar o componente FavoriteRecipes aparece o título Favorite Recipes', () => {
+    const { history } =  renderWithRouter(<App />);
+    history.push('/favorite-recipes');
+
+    const favoriteRecEl = screen.getByRole('heading', {  name: /favorite recipes/i})
+    expect(favoriteRecEl).toBeInTheDocument();
+  });
+  
+  it('Verifica se ao clicar no food-bottom-btn é redirecionado para página /foods', () => {
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorite-recipes');
+   
+    // <button type="button" data-testid="profile-top-btn" src="./images/profileIcon.svg"><img src="/static/media/profileIcon.44eb3608f431845fe2fc2d2a23d758ae.svg" alt="Profile"></button>
+    const profileIconEl = screen.getByTestId(/profile-top-btn/i);
+        
+    expect (profileIconEl).toBeInTheDocument();
+
+    userEvent.click(profileIconEl);
+    const { pathname } = history.location;
+        expect(pathname).toBe('/profile');
+  });
+
+  it('Verifica "unfavorite button"', async () => {
+    localStorage.setItem('favoriteRecipes', favoriteRecipes);
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorite-recipes');
+
+    const firstFavButton = await screen.findByTestId('0-horizontal-favorite-btn');
+    userEvent.click(firstFavButton);
+
+    const recipesTitles = await screen.findAllByTestId(/-horizontal-name/);
+    expect(recipesTitles.length).toBe(1);
+  });
+
+  it('Verifica o filtro "food"', async () => {
+    localStorage.setItem('favoriteRecipes', favoriteRecipes);
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorite-recipes');
+
+    const foodFilter = await screen.findByTestId('filter-by-food-btn');
+    userEvent.click(foodFilter);
+
+    const recipesTitles = await screen.findAllByTestId(/-horizontal-name/);
+    expect(recipesTitles.length).toBe(1);
+  });
+
+  it('Verifica o filtro "drink"', async () => {
+    localStorage.setItem('favoriteRecipes', favoriteRecipes);
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorite-recipes');
+
+    const drinkFilter = await screen.findByTestId('filter-by-drink-btn');
+    userEvent.click(drinkFilter);
+
+    const recipesTitles = await screen.findAllByTestId(/-horizontal-name/);
+    expect(recipesTitles.length).toBe(1);
+  });
+
+  it('Verifica o filtro "all"', async () => {
+    localStorage.setItem('favoriteRecipes', favoriteRecipes);
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorite-recipes');
+
+    const allFilter = await screen.findByTestId('filter-by-all-btn');
+    userEvent.click(allFilter);
+
+    const recipesTitles = await screen.findAllByTestId(/-horizontal-name/);
+    expect(recipesTitles.length).toBe(2);
+  });
+
+  it('Verifica caso o localStorage esteja vazio', async () => {
+    localStorage.removeItem('favoriteRecipes');
+    const { history } = renderWithRouter(<App />);
+    history.push('/favorite-recipes');
+
+    const recipesTitles = screen.queryByTestId('0-horizontal-name');
+    expect(recipesTitles).not.toBeInTheDocument();
+  });
+});
