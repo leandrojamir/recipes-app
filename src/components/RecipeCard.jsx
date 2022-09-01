f6e978cbec6cd44840d4d2d54f7afc47e67dd4a1
Raissa Vasconcelos mendes <rai.celos20@gmail.com>
2022-09-01 18:33:11 -0300

    Realizado correçãode conflito de novo.

--- src/components/RecipeCard.jsx
+++ src/components/RecipeCard.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const RecipeCard = (props) => {
   const { img, name, key, index } = props;
   // key recebe id2
   return (
-
     <div data-testid={ `${index}-recipe-card` }>
-      <img src={ img } alt={ name } data-testid={ `${index}-card-img` } />
+      <Link key={ key } to={ `/drinks/${key}` }>
+        <img
+          src={ img }
+          alt={ name }
+          data-testid={ `${index}-card-img` }
+        />
+      </Link>
       <p data-testid={ `${index}-card-name` }>{name}</p>
     </div>
   );
