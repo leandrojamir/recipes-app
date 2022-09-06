cc568d15de4e845ad3d8b94da096cc015c031a89
leandrojamir <leandrojamir@hotmail.com>
2022-09-06 11:55:06 -0300

    adcionando testes da 44 a 49 e testes da 37 a 43

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -9,10 +9,97 @@
 //   RecipesInProgress.jsx      |       0 |      100 |       0 |       0 | 4                           
 //  src/Pages                   |    84.9 |    91.66 |   66.66 |    84.9 |                             
 //   DoneRecipes.jsx            |     100 |      100 |     100 |     100 |                   
-import React from 'react';
+
+import React from "react";
+import { screen } from '@testing-library/react'
+import renderWithRouter from "./helpers/renderWithRouter";
+import userEvent from '@testing-library/user-event';
+import App from "../App";
+
+const doneRecipes = JSON.stringify([
+  {
+    id: '52771',
+    type: 'food',
+    nationality: 'Italian',
+    category: 'Vegetarian',
+    alcoholicOrNot: '',
+    name: 'Spicy Arrabiata Penne',
+    image: 'https://www.themealdb.com/images/media/meals/ustsqw1468250014.jpg',
+    doneDate: '23/06/2020',
+    tags: ['Pasta', 'Curry'],
+  },
+  {
+    id: '178319',
+    type: 'drink',
+    nationality: '',
+    category: 'Cocktail',
+    alcoholicOrNot: 'Alcoholic',
+    name: 'Aquamarine',
+    image: 'https://www.thecocktaildb.com/images/media/drink/zvsre31572902738.jpg',
+    doneDate: '23/06/2020',
+    tags: [],
+  },
+]);
 
 describe('Aqui será o test da doneRecipes quando incluir req 48 e 49 e localStorage', () => {
   it('por enquanto já esta sendo coberto 100% pelo SearchBar.test', () => {
 
+  });
+  it(' Titulo e direcionamento profile', () => {
+    const { history } =  renderWithRouter(<App />);
+    history.push('/done-recipes');
+    // <h1 data-testid="page-title">Favorite Recipes</h1>
+    const tituloHeader = screen.getByTestId(/page-title/i)
+    expect(tituloHeader).toBeInTheDocument();
+    expect(tituloHeader).toHaveTextContent(/Done Recipes/i)
+   
+    // <button type="button" data-testid="profile-top-btn" src="./images/profileIcon.svg"><img src="/static/media/profileIcon.44eb3608f431845fe2fc2d2a23d758ae.svg" alt="Profile"></button>
+    const profileBtn = screen.getByTestId(/profile-top-btn/i);        
+    expect (profileBtn).toBeInTheDocument();
+    userEvent.click(profileBtn);
+    expect(history.location.pathname).toBe('/profile');
+  });
+
+  it('Verifica o filtros', async () => {
+    localStorage.setItem('doneRecipes', doneRecipes);
+    const { history } = renderWithRouter(<App />);
+    history.push('/done-recipes');
+
+    const foodFilter = await screen.findByTestId(/filter-by-food-btn/i);
+    userEvent.click(foodFilter);
+    // <h3 data-testid="0-horizontal-name">Spicy Arrabiata Penne</h3>
+    const horizontalNames = await screen.findAllByTestId(/horizontal-name/);
+    // expect(recipesTitles).toHaveTextContent(/Spicy Arrabiata Penne/i)
+    expect(horizontalNames.length).toBe(1);
+ 
+    const drinkBtn = await screen.findByTestId('filter-by-drink-btn');
+    userEvent.click(drinkBtn);
+    // <h3 data-testid="0-horizontal-name">Aquamarine</h3>
+    expect(horizontalNames.length).toBe(1);
+  });
+
+  it('testa all', async () => {
+    localStorage.setItem('doneRecipes', doneRecipes);
+    const { history } = renderWithRouter(<App />);
+    history.push('/done-recipes');
+    // <img data-testid="0-horizontal-favorite-btn" src="/static/media/blackHeartIcon.083cb006913d197c95857ebfa2161db7.svg" alt="back heart"></img>
+    // const favoriteBtn0 = await screen.findByTestId('0-horizontal-favorite-btn');
+    // userEvent.click(favoriteBtn0);
+
+    const horizontalNames = await screen.findAllByTestId(/horizontal-name/);
+    // expect(horizontalNames.length).toBe(1);
+    
+    const allBtn = await screen.findByTestId('filter-by-all-btn');
+    userEvent.click(allBtn);
+    expect(horizontalNames.length).toBe(2);
+  });
+
+  it('remove localStorage', async () => {
+    localStorage.removeItem('doneRecipes');
+    const { history } = renderWithRouter(<App />);
+    history.push('/done-recipes');
+
+    const horizontalNames = screen.queryByTestId('0-horizontal-name');
+    expect(horizontalNames).not.toBeInTheDocument();
   });
 });
